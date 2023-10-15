9912458c57d0e91aacec79d357a50430aeea58c9
Faris <faris.leventa@stu.ibu.edu.ba>
2023-10-15 17:25:01 +0200

    Task 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -44,3 +44,25 @@ import java.util.Scanner;
     }
 }*/
 
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner reader=new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        int num1=reader.nextInt();
+
+        System.out.println("Enter second number");
+        int num2=reader.nextInt();
+
+        System.out.println("Enter third number");
+        int num3=reader.nextInt();
+
+        int sum=num1+num2+num3;
+
+        System.out.println("The sum is "+sum);
+
+    }
+}
