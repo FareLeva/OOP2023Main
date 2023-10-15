f9fe4571721470a6b705519b234a42f112a9effd
Faris <faris.leventa@stu.ibu.edu.ba>
2023-10-15 17:19:11 +0200

    Task 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,46 @@
 package org.example;
+import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+
+
+
+/*public class Main {
     public static void main(String[] args) {
-      
+        int counter=0;
+        int maxattempt=5;
+        String password="FareLeva0";
+
+        Scanner reader=new Scanner(System.in);
+
+
+        while (counter<maxattempt){
+            System.out.println("Enter password");
+            String userpass=reader.nextLine();
+
+            if (userpass.equals(password)){
+                System.out.println("Congrats, Access Granted");
+                break;
+            }
+            else {
+                System.out.println("Nope try again");
+                userpass=reader.nextLine();
+                counter+=1;
+            }
+
+
+
+        }
+        if (counter==maxattempt){
+            System.out.println("Max attempts reached");
+
+
+        };
+
+
+
+
+
+
     }
-}
+}*/
+
